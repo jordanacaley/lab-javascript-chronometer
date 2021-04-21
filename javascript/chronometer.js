046b7901766ed8992c91ccc343451c0a0754513e
Jordan Caley <jordancaley10@gmail.com>
2021-04-21 16:28:10 +0200

    startClick & getMinutes

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,12 +1,20 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = 0;
   }
+
   startClick(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime++;
+      if (callback) {
+        callback();
+      }
+    }, 1000);
   }
+
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
   getSeconds() {
     // ... your code goes here
